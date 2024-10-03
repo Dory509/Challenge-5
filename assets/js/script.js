1669e98da358aa1093a7a2629ea90e56e04d33f5
Dory509 <dorycelestin@outlook.com>
2024-10-03 11:12:32 -0400

    Create a task card

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,36 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+var taskCard=`<div class="card" id=${task.id}style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${task.title}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">${task.description}</h6>
+    <p class="card-text">Due Date:${task.deadline}</p>
+    <button class="btn btn-danger" data-id=${task.id}>Delete task<button>
+  </div>
+</div>`
+return taskCard
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-   
+    var previousTaskList = JSON.parse(localStorage.getItem("taskboard")) || []
+    $("#todo-cards").html("")
+    $("#in-progress-cards").html("")
+    $("#done-cards").html("")
+    
+    previousTaskList.forEach(element => {
+        var taskCard = createTaskCard(element)
+        if(element.status =="to-do"){
+            $("#todo-cards").append(taskCard)
+        }
+         else if(element.staus== "in-progress"){
+            $("#in-progress-cards").append(taskCard)
+         }
+         else {
+         $("#done-cards").append(taskCard)
+         }
+    });
 }
 
 // Todo: create a function to handle adding a new task
@@ -33,6 +57,7 @@ function handleAddTask(event){
     $("#taskTitle").val("");
     $("#taskDescription").val("");
     $("#taskDeadline").val("");
+    renderTaskList()
 }
 
 // Todo: create a function to handle deleting a task
@@ -48,7 +73,7 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 $("#saveTask").on("click",handleAddTask)
-
+renderTaskList()
 
 });
 
