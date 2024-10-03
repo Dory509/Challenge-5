12dc1039a5d2e1eefb232c6c297924483cc36ecd
Dory509 <dorycelestin@outlook.com>
2024-10-03 10:49:10 -0400

    complete handle add task function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    return Math.floor(Math.random()*1000)
 }
 
 // Todo: create a function to create a task card
@@ -14,12 +14,25 @@ function createTaskCard(task) {
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+   
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-
+    var newtask={ 
+        id: generateTaskId(),
+        title: $("#taskTitle").val(),
+        description:$("#taskDescription").val(),
+        deadline:$("#taskDeadline").val(),
+        status:"to-do"
+        };
+    var previousTaskList = JSON.parse(localStorage.getItem("taskboard")) || []
+    previousTaskList.push(newtask)
+    localStorage.setItem("taskboard",JSON.stringify(previousTaskList))
+    alert("Added task")
+    $("#taskTitle").val("");
+    $("#taskDescription").val("");
+    $("#taskDeadline").val("");
 }
 
 // Todo: create a function to handle deleting a task
@@ -34,5 +47,9 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+$("#saveTask").on("click",handleAddTask)
+
 
 });
+
+
